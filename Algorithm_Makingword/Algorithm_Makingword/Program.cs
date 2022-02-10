e26d6b596b12c0d8d57e3a8c0dabcf37f9a4e968
Kim Jewon <rlawpdnjs1998@naver.com>
2022-02-10 15:08:57 +0900

    프로젝트 시작
    단어 만들기 알고리즘

---
+++ Algorithm_Makingword/Algorithm_Makingword/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorithm_Makingword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
--- Algorithm_startup/Algorithm_startup/Program.cs
+++ Algorithm_startup/Algorithm_startup/Program.cs
@@ -1,15 +1,54 @@
-﻿#define DEBUG
-
-using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 using static System.Console;
 namespace Algorithm_startup
 {
+    class Gamer
+    {
+        public Queue<char> que;
+
+        public Gamer(string str, bool isDescending)
+        {
+            que = new Queue<char>();
+
+            // 우선순위에 따라 정렬
+            var linq = str.OrderBy((x) => x);
+
+            if (isDescending)
+                linq = str.OrderByDescending((x) => x);
+
+            // 큐에 삽입
+            foreach (var ssss in linq)
+                que.Enqueue(ssss);
+
+        }
+    }
+ 
+
     class Program
     {
+ 
+
         static void Main(string[] args)
         {
             // 시작
-           Manager manager = new Manager();
+            Manager manager = new Manager();
+
+
+            //Gamer p1 = new Gamer("startlink", false);
+            //Gamer p2 = new Gamer("startlink", true);
+
+
+            //string temp = "";
+            //for (int i = 0; i < "startlink".Length; i++)
+            //{
+            //    if (i % 2 == 0)
+            //        temp += p1.que.Dequeue();
+            //    else
+            //        temp += p2.que.Dequeue();
+
+            //}
+            //WriteLine(temp);
         }
     }
 }
